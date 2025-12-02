61f93609427145edc6231e9cf570807d0f4ac0c4
Rodrigo Garcia Couto <rodrigo.couto@ibm.com>
2025-12-01 21:07:56 -0300

    Refactor 2025 Day 01

--- solutions/2025/day_01/solution.py
+++ solutions/2025/day_01/solution.py
@@ -68,31 +68,22 @@ class Solution(StrMatchRegexpSolution):
             direction = rotation[0]
             clicks = int(rotation[1])
 
-            self.debug(f"1 ==> Dir: {direction}, Clk: {clicks}, Dial: {
-                       dial}, C1: {count1}, C2: {count2}")
-
             dial_ant = dial
 
             count2 += clicks // 100
             dial += (1 if direction == "R" else -1) * (clicks % 100)
 
             if dial == 0 or dial == 100:
-                self.debug("Dial at 0")
                 dial = 0
                 count1 += 1
                 count2 += 1
             elif dial < 0:
-                self.debug("Dial crossed 0")
                 dial += 100
                 if dial_ant != 0:
                     count2 += 1
             elif dial > 99:
-                self.debug("Dial crossed 0")
                 dial -= 100
                 if dial_ant != 0:
                     count2 += 1
-
-            self.debug(f"2 ==> Dial: {dial}, C1: {count1}, C2: {count2}")
-            self.debug("------------------")
 
         return (count1, count2)
