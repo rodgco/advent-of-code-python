d3866fe4ffabcf9d20caa5aa62bb5a9c27f90097
Rodrigo Garcia Couto <rodrigo.couto@ibm.com>
2025-12-03 14:05:14 -0300

    2025 Day 03 Full

--- solutions/2025/day_03/solution.py
+++ solutions/2025/day_03/solution.py
@@ -1,8 +1,24 @@
-# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+#  Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
 
 # puzzle prompt: https://adventofcode.com/2025/day/3
 
 from ...base import AnyFunSolution
+
+
+def remove_battery(bank):
+    battery_to_remove = None
+    for index, first_rating in enumerate(bank):
+        if index == len(bank)-1:
+            break
+        second_rating = bank[index + 1]
+        if first_rating < second_rating:
+            battery_to_remove = index
+            break
+
+    if battery_to_remove == None:
+        battery_to_remove = len(bank) - 1
+
+    del bank[battery_to_remove]
 
 
 class Solution(AnyFunSolution):
@@ -20,7 +36,7 @@ class Solution(AnyFunSolution):
             max_joltage = 0
             batteries_count = len(bank)
             for index, first_rating in enumerate(bank):
-                for second_rating in bank[index + 1 :]:
+                for second_rating in bank[index + 1:]:
                     joltage = first_rating * 10 + second_rating
                     max_joltage = max(joltage, max_joltage)
 
@@ -31,7 +47,14 @@ class Solution(AnyFunSolution):
     # @answer(1234)
 
     def part_2(self) -> int:
-        pass
+        total_joltage = 0
+        for bank in self.input:
+            batteries_in_bank = len(bank)
+            for i in range(1, batteries_in_bank-11):
+                remove_battery(bank)
+            joltage = int("".join([str(i) for i in bank]))
+            total_joltage += joltage
+        return total_joltage
 
     # @answer((1234, 4567))
     # def solve(self) -> tuple[int, int]:
