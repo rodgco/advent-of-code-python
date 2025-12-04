cf842b7d94f3a9f46adea9e22003dfe231451131
Rodrigo Couto <r@rodg.co>
2025-12-04 00:58:49 -0300

    Refactor 2025 Day 2

--- solutions/2025/day_02/solution.py
+++ solutions/2025/day_02/solution.py
@@ -3,8 +3,8 @@
 # puzzle prompt: https://adventofcode.com/2025/day/2
 
 from ...base import IntSplit2RowSolution, answer
-from ...utils.text_utils import split_into_chunks
-from ...utils.arr_utils import all_equal
+import re
+
 
 class Solution(IntSplit2RowSolution):
     _year = 2025
@@ -13,39 +13,15 @@ class Solution(IntSplit2RowSolution):
     separator = ","
     separator2 = "-"
 
-    # @answer(1234)
-    def part_1(self) -> int:
-        count = 0
+    def solve(self) -> tuple[int, int]:
+        count1 = 0
+        count2 = 0
         for rng in self.input:
-            for id in range(rng[0], rng[1]+1):
-                id_str = str(id)
+            for id in range(rng[0], rng[1] + 1):
                 self.spinner()
-                try:
-                    arr = split_into_chunks(id_str, 2)
-                    if all_equal(arr):
-                        count += id
-                except ValueError:
-                    pass
-        return count
-
-    # @answer(1234)
-    def part_2(self) -> int:
-        count = 0
-        for rng in self.input:
-            for id in range(rng[0], rng[1]+1):
                 id_str = str(id)
-                for chunks in range(2, len(id_str)+1):
-                    self.spinner()
-                    try:
-                        arr = split_into_chunks(id_str, chunks)
-                        if all_equal(arr):
-                            count += id
-                            break
-                    except ValueError:
-                        pass
-
-        return count
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+                if re.search(r"^(\d+)\1$", id_str):
+                    count1 += id
+                if re.search(r"^(\d+)\1+$", id_str):
+                    count2 += id
+        return (count1, count2)
